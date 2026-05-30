07cbba2930ebadef4857d6795033c3f0abe42f16
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 16:18:33 -0300

    feat: Sessão de reabilitação, entidade de telemtria

---
+++ void/void/src/main/java/br/com/fiap/model/SessaoReabilitacao.java
@@ -0,0 +1,24 @@
+package br.com.fiap.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_VOID_SESSAO_REABILITACAO")
+public class SessaoReabilitacao {
+
+    @EmbeddedId
+    private SessaoReabilitacaoId id;
+
+    // Percentual de desgaste captado pelo Arduino/ESP32 (sensores)
+    private Double desgasteAcumulado;
+
+    // Flag que será validada no PL/SQL com IF/ELSE para travar o treino
+    private Boolean alertaFadigaCritica;
+
+    @ManyToOne
+    @MapsId("pacienteId")
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+}
