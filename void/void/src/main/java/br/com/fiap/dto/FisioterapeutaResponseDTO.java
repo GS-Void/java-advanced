b004c85d83de0e86eb876010e07ed1a1e8714bc7
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 16:45:11 -0300

    feat: DTO de saida do Fisioterapeuta

---
+++ void/void/src/main/java/br/com/fiap/dto/FisioterapeutaResponseDTO.java
@@ -0,0 +1,12 @@
+package br.com.fiap.dto;
+
+import lombok.Data;
+
+@Data
+public class FisioterapeutaResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String registroProfissional;
+
+}
