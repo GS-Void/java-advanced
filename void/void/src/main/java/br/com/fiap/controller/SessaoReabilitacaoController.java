30035c90f1454434f7308e1afda02cf1032680cd
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 16:33:01 -0300

    feat: Adicionando SessaoReabilitacaoController, configurando rotas URLs

--- void/void/src/main/java/br/com/fiap/controller/SessaoReabilitacaoController.java
+++ void/void/src/main/java/br/com/fiap/controller/SessaoReabilitacaoController.java
@@ -1,4 +1,28 @@
 package br.com.fiap.controller;
 
+import br.com.fiap.model.SessaoReabilitacao;
+import br.com.fiap.service.SessaoReabilitacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sessoes")
 public class SessaoReabilitacaoController {
+
+    @Autowired
+    private SessaoReabilitacaoService service;
+
+    @PostMapping
+    public ResponseEntity<SessaoReabilitacao> criar(@RequestBody SessaoReabilitacao sessao) {
+        return ResponseEntity.ok(service.salvar(sessao));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SessaoReabilitacao>> listarTodas() {
+        return ResponseEntity.ok(service.listarTodas());
+    }
+    
 }
