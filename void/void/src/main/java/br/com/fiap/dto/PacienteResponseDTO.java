935e5813b9482c2532cd29e58c0db6fc680dcad5
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 16:42:50 -0300

    feat: DTO de saida do Paciente

--- void/void/src/main/java/br/com/fiap/dto/PacienteResponseDTO.java
+++ void/void/src/main/java/br/com/fiap/dto/PacienteResponseDTO.java
@@ -1,4 +1,12 @@
 package br.com.fiap.dto;
 
+import lombok.Data;
+
+@Data
 public class PacienteResponseDTO {
+
+    private Long id;
+    private String nome;
+    private Double limiteEsforcoCritico;
+
 }
