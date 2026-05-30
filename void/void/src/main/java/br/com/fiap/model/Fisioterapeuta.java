56bf171e4f4f59aaf4bd0669617380640f229133
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 16:16:27 -0300

    feat: Classe Fisioterapeuta com registro profissional

--- void/void/src/main/java/br/com/fiap/model/Fisioterapeuta.java
+++ void/void/src/main/java/br/com/fiap/model/Fisioterapeuta.java
@@ -1,4 +1,15 @@
 package br.com.fiap.model;
 
-public class Fisioterapeuta {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "TB_VOID_FISIOTERAPEUTA")
+public class Fisioterapeuta extends Usuario {
+    
+    private String registroProfissional;
 }
