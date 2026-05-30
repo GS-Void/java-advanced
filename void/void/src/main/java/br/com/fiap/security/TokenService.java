31a25690c75a3e36b2347f3d0c837ac02e243978
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 17:06:07 -0300

    feat: Criando token do projeto

---
+++ void/void/src/main/java/br/com/fiap/security/TokenService.java
@@ -0,0 +1,49 @@
+package br.com.fiap.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    // Senha secreta da API 
+    private static final String SECRET = "FIAP_VOID_SECRET_KEY_123";
+
+    public String gerarToken(String email) {
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256(SECRET);
+            return JWT.create()
+                    .withIssuer("API VOID")
+                    .withSubject(email)
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256(SECRET);
+            return JWT.require(algoritmo)
+                    .withIssuer("API VOID")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
+    private Instant dataExpiracao() {
+        // Token expira em 2 horas
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
