9bb8fdeab44f0f111499cbe8c7c5f32efd76ed74
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 16:28:50 -0300

    feat: Adicionando SessaoReabilitacaoService para CRUD

--- void/void/src/main/java/br/com/fiap/service/SessaoReabilitacaoService.java
+++ void/void/src/main/java/br/com/fiap/service/SessaoReabilitacaoService.java
@@ -1,4 +1,45 @@
 package br.com.fiap.service;
 
+import br.com.fiap.model.SessaoReabilitacao;
+import br.com.fiap.model.SessaoReabilitacaoId;
+import br.com.fiap.repository.SessaoReabilitacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class SessaoReabilitacaoService {
+
+    @Autowired
+    private SessaoReabilitacaoRepository repository;
+
+    public SessaoReabilitacao salvar(SessaoReabilitacao sessao) {
+        return repository.save(sessao);
+    }
+
+    public List<SessaoReabilitacao> listarTodas() {
+        return repository.findAll();
+    }
+
+    public Optional<SessaoReabilitacao> buscarPorId(SessaoReabilitacaoId id) {
+        return repository.findById(id);
+    }
+
+    public SessaoReabilitacao atualizar(SessaoReabilitacaoId id, SessaoReabilitacao sessaoAtualizada) {
+        if (repository.existsById(id)) {
+            sessaoAtualizada.setId(id);
+            return repository.save(sessaoAtualizada);
+        }
+        return null;
+    }
+
+    public boolean deletar(SessaoReabilitacaoId id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
