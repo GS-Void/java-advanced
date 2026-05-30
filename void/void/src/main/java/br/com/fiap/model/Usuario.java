422d29c2a341d0fb51aa31e9e7c961fa2fc82648
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 16:11:43 -0300

    feat: Classe pai abstrata

--- void/void/src/main/java/br/com/fiap/model/Usuario.java
+++ void/void/src/main/java/br/com/fiap/model/Usuario.java
@@ -1,4 +1,24 @@
 package br.com.fiap.model;
 
-public class Usuario {
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_VOID_USUARIO")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String email;
 }
