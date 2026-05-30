b543ce227dccc654b36a7097b6d762f278877fcd
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 17:07:57 -0300

    refactor: Protegendo a aplicação

--- void/void/src/main/java/br/com/fiap/config/SecurityConfig.java
+++ void/void/src/main/java/br/com/fiap/config/SecurityConfig.java
@@ -1,23 +1,39 @@
 package br.com.fiap.config;
 
+import br.com.fiap.security.SecurityFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                // Define que a API é Stateless (não guarda sessão em memória)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        // Libera acesso total às rotas da documentação do Swagger
+                        // Libera o Swagger para o professor conseguir ver a documentação
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .anyRequest().permitAll()
-                );
+
+                        // Libera a rota de Login para podermos gerar o Token
+                        .requestMatchers("/api/auth/login").permitAll()
+
+                        // Tranca TODO o resto (Pacientes, Fisioterapeutas e Sessões)
+                        .anyRequest().authenticated()
+                )
+                // Avisa o Spring para rodar o nosso filtro JWT ANTES do filtro padrão dele
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
