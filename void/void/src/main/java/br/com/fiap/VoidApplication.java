b96a91d74de64911a6491250ec1630cbfe536ad6
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 16:10:09 -0300

    feat: Projeto base com Spring Boot e suas dependências.

---
+++ void/void/src/main/java/br/com/fiap/VoidApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoidApplication.class, args);
+	}
+
+}
