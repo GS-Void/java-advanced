eec590f9d24b89ea2c549fab261987dfa9643cd8
hrqmartins <hrqmartins129@gmail.com>
2026-05-30 16:22:16 -0300

    feat: FisioterapeutaRepository que permite salvar e buscar os dados dos pacientes

---
+++ void/void/src/main/java/br/com/fiap/repository/FisioterapeutaRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.repository;
+
+import br.com.fiap.model.Fisioterapeuta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FisioterapeutaRepository extends JpaRepository<Fisioterapeuta, Long> {
+}
